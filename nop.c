a9d8f1b4b5dbf619ce4a8531c659b2dceb94556b
Hector Macassa <cbmacassa@gmail.com>
2024-01-18 03:43:49 -0800

    5. nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_nop - Does nothing.
+ * @head: Stack head
+ * @counter: line_number
+ *
+ * Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
